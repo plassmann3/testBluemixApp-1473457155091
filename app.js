a352380843f119d883526395c7b55472b397ccc7
charlie cox <charlie.cox@ibm.com>
2016-09-09 13:45:29 -0500

    Firing pipeline

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get("/", function(req, res) {
-  res.end("all is good");
+  res.end("all is good with the app");
 });
 
 app.listen(process.env.VCAP_APP_PORT || 4000);
